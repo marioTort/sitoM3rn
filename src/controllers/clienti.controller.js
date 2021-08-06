fdc8a3d00adc6af497918a9239ddbf0146c1df30
giuseppesign <s.giuseppe1199@gmail.com>
2021-08-06 15:55:00 +0200

    Update clienti.controller.js
    ciao

--- src/controllers/clienti.controller.js
+++ src/controllers/clienti.controller.js
@@ -5,9 +5,15 @@ module.exports = {
     index(req, res) {
         res.json({
             message:'Hello world'
-        });  
+        });
     },
     create(req, res) {
-        const { nomeCliente, cognomeCliente, dataNascita, nTelefono, codiceFiscale, nPatente, tipoPatente, email, password }
+        const { nomeCliente, cognomeCliente, dataNascita, nTelefono, codiceFiscale, nPatente, tipoPatente, email, password } = req.body;
+        let data = {};
+        let user = Cliente.findOne({email});
+        if(!user){
+            data = {nomeCliente, cognomeCliente, dataNascita, nTelefono, codiceFiscale, nPatente, tipoPatente, email, password};
+            user = Cliente.create(data);
+        }
     }
 }
