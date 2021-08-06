b4462c332c2c84c18158458a4b4510e05ee1747d
giuseppesign <s.giuseppe1199@gmail.com>
2021-08-06 16:25:02 +0200

    update
    abc

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,10 @@ routes.get('/', function (req, res) {
     res.json({ message: 'Hello world' });
 });
 
+
+
 routes.post('/api/clienti', Cliente.create);
+routes.get('/api/clienti', Cliente.index);
+routes.get('/api/clienti.dettagli', Cliente.dettagli);
 
 module.exports = routes;
